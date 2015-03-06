d4abab227945474353e9b35bad42d2a750caa188
Sarah Ing <sarahikeda@gmail.com>
2015-03-06 14:19:07 -0600

    start of routes for questions and response

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,10 @@ Rails.application.routes.draw do
 
 
   root to: 'static#index'
+  get '/' => 'questions#index', as: :index
+  resources :questions do
+    resources :responses 
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
