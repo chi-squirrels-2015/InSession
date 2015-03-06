73700ab70a403a27b3aee08c2ddceb58df958c96
J-D <thejdavid@users.noreply.github.com>
2015-03-06 14:47:29 -0600

    fixing heroku

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,4 @@
 //= require_tree .
 //= require private_pub
 //= require chat
+
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,7 +14,6 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
-
   config.serve_static_assets = true
 
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
