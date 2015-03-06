a620fc6570417357bfbf7922e54a780246dfba22
Daniel Hanson & J-D <pair+hanieldanson.thejdavid@devbootcamp.com>
2015-03-06 10:33:05 -0600

    devise done+basic layout

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,4 +1,4 @@
-class StaticController < ActionController::Base
+class StaticController < ApplicationController
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
