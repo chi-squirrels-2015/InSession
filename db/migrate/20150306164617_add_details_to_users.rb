d65fec3b68f0a9cf3fd086d2284245ca4253c1a4
Sarah Ing <sarahikeda@gmail.com>
2015-03-06 13:37:22 -0600

    add seeds

--- db/migrate/20150306164617_add_details_to_users.rb
+++ db/migrate/20150306164617_add_details_to_users.rb
@@ -1,8 +1,7 @@
 class AddDetailsToUsers < ActiveRecord::Migration
   def change
-    add_column :email, :string
-    add_column :street_address, :string
-    add_column :city, :string
-    add_column :zip_code, :integer
+    add_column :users, :street_address, :string
+    add_column :users, :city, :string
+    add_column :users, :zip_code, :integer
   end
 end
