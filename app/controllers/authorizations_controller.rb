177f9b31c5f9ecddb0c6e53ebbe32d8b1c9abd2f
John Schulz <johnschulz75@gmail.com>
2015-03-07 16:20:51 -0600

    OAuth is working on a base level

--- app/controllers/authorizations_controller.rb
+++ app/controllers/authorizations_controller.rb
@@ -7,11 +7,12 @@ class AuthorizationsController < ApplicationController
   def create
     auth_hash = request.env['omniauth.auth']
     p auth_hash.credentials # Contains the token and secret
+    user = current_user
     if current_user
       # Means our user is signed in. Add the authorization to the user
-      current_user.add_provider(auth_hash)
-
-      render :text => "You can now login using #{auth_hash["provider"].capitalize} too!"
+      req = user.oauth_request(auth_hash)
+      render :data_retrieval, locals: {provider: Provider.find_by(name: auth_hash[:provider]), req: req}
+      # render :text => "#{current_user.first_name} can now login using #{auth_hash["provider"].capitalize} too!"
     else
       # Log him in or sign him up
       auth = Authorization.find_or_create(auth_hash)
@@ -22,4 +23,5 @@ class AuthorizationsController < ApplicationController
       render :text => "Welcome #{auth.user.first_name auth.user.last_name}!"
     end
   end
+
 end
--- app/models/authorization.rb
+++ app/models/authorization.rb
@@ -3,10 +3,20 @@ class Authorization < ActiveRecord::Base
   belongs_to :provider
   validates :provider, :uid, presence: true
 
+  def self.find_by_oauth_credentials(auth_hash)
+# ASSIGN the auth_hash params to local variables
+  provider = auth_hash[:provider]
+  uid = auth_hash[:uid]
+# FIND the record containing the two variables in the Authorizations table
+  credentials = Authorization.where(:provider=> Provider.find_by(name: provider)).where(:uid => uid)
+# RETURN the record or nil
+  end
+
   def self.find_or_create(auth_hash)
-    unless auth = find_by_provider_and_uid(auth_hash["provider"], auth_hash["uid"])
+    unless auth = find_by_oauth_credentials(auth_hash)
       user = User.find(current_user.id)
-      auth = create user: user, provider: auth_hash["provider"], uid: auth_hash["uid"]
+      provider = Provider.find_by(name: auth_hash[:provider])
+      auth = create :user => self, :provider => provider, :uid => auth_hash[:uid]
     end
 
     auth
