f30913d0825a3f68be72e92555d1643994873397
soborok <soborok@gmail.com>
2015-03-08 14:53:19 -0500

    refactored css and js of map

--- db/schema.rb
+++ db/schema.rb
@@ -132,7 +132,6 @@ ActiveRecord::Schema.define(version: 20150308190126) do
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
     t.string   "last_name"
-    t.string   "name"
     t.integer  "course_id"
     t.text     "bio"
     t.string   "preferred_language"
