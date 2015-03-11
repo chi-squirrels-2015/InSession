415ac06741c614534b3e30cfb37edc0d7b3e7856
J-D <thejdavid@users.noreply.github.com>
2015-03-11 10:56:26 -0500

    add new meetup view

--- db/migrate/20150306032107_create_meetups.rb
+++ db/migrate/20150306032107_create_meetups.rb
@@ -3,8 +3,9 @@ class CreateMeetups < ActiveRecord::Migration
     create_table :meetups do |t|
       t.references :course, index: true     
       t.references :organizer, index: true 
-      t.datetime  :begin_time
-      t.datetime  :end_time
+      t.time  :begin_time
+      t.time  :end_time
+      t.date  :scheduled_date
       t.references :venue, index: true
       t.boolean   :remote
       t.string    :title
