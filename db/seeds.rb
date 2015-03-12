55bf89b3e174b5eb133647833ed7772de2cb3182
hanieldanson <hanson.e.daniel@gmail.com>
2015-03-12 13:28:52 -0500

    fix seed, remove completed courses from users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,22 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-
-# require 'faker'
-
 Meetup.__elasticsearch__.delete_index!
 Question.__elasticsearch__.delete_index!
 Exercise.__elasticsearch__.delete_index!
 UserExercise.__elasticsearch__.delete_index!
 
-
 bio = "Biltong jerky beef prosciutto, filet mignon boudin sirloin ham hock chuck. Ball tip fatback andouille frankfurter beef ribs. Brisket venison capicola ball tip tenderloin cupim chicken, meatloaf ground round prosciutto bresaola cow turducken boudin."
-
-
 
 sarah = User.create(first_name:"Sarah", last_name: "Ing", email:"chocolate@gmail.com", password:"chocolate", street_address: "351 West Hubbard Street", city: "Chicago", state: "IL", zip: 60654, max_distance: 5, bio: "I am eager to see what this programming business is about! Willing to learn from anyone with more experience.")
 matthew = User.create(first_name:"Matthew", last_name: "Who", email:"matthewwho@me.com", password:"password", street_address: "351 West Hubbard Street", city: "Chicago", state: "IL", zip: 60654, max_distance: 5, bio: bio)
@@ -47,29 +34,19 @@ testing = Course.create(title:"Testing in Ruby on Rails", organization: "Khan Ac
 
 courses = Course.all
 users = User.all
+
 150.times do
   venue = Venue.find(rand(Venue.count)+1)
   base_time = Faker::Time.forward(rand(60)+1, :evening)
   Meetup.create(title: Faker::Company.name, description: Faker::Hacker.say_something_smart, course: courses.sample, organizer: users.sample, venue: venue, remote: true, latitude: venue.latitude, longitude: venue.longitude, scheduled_date: base_time, begin_time: base_time, end_time: base_time + 3600)
-end
-
-ruby_newbies = Meetup.create(title: "Ruby For Newbies", course: newbies, description: "This is our first meetup for beginners who are interested in learning Ruby. We will take the time to go over some programming basics and answer any questions you have. Food will be provided!", organizer: daniel, venue: venue1, remote: true, latitude: venue1.latitude, longitude: venue1.longitude, scheduled_date: Time.now, begin_time: Time.now, end_time: Time.now + rand(1..4).days)
-
-#NOTHING LESS THAN SLEEP 0.2!!! Google gets pissy otherwise.
-200.times do
-  user = user_array.sample
-  User.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: [Faker::Internet.free_email, Faker::Internet.email, Faker::Internet.safe_email].sample, password: 'password', bio: bio, preferred_language: "English", street_address: user[:address], city: user[:city], state: user[:state], zip: user[:zip], max_distance: rand(5...20))
   sleep 0.2
 end
 
+venue1 = venues_array.sample
 
-# 150.times do
-#   Question.create(title: Faker::Hacker.say_something_smart, content: Faker::Hacker.say_something_smart, user: users.sample, course: courses.sample)
-# end
+ruby_newbies = Meetup.create(title: "Ruby For Newbies", course: newbies, description: "This is our first meetup for beginners who are interested in learning Ruby. We will take the time to go over some programming basics and answer any questions you have. Food will be provided!", organizer: daniel, venue: venue1, remote: true, latitude: venue1.latitude, longitude: venue1.longitude, scheduled_date: Time.now, begin_time: Time.now, end_time: Time.now + rand(1..4).days)
 
-# 150.times do
-#   Question.create(title: Faker::Hacker.say_something_smart, content: Faker::Hacker.say_something_smart, user: users.sample, course: courses.sample)
-# end
+meetups = Meetup.all
 
 array_of_question = ["How do I use css?",
 "Can I use css for styling web pages",
@@ -110,32 +87,6 @@ array_of_question = ["How do I use css?",
 "How is cheese made?",
 "What is the real story behind Cinco de Mayo?",
 "When will DBC's website be rebuilt with Meteor?"]
-# 39.times do
-#   Question.create(title: array_of_question.pop, content: Faker::Hacker.say_something_smart, user: users.sample, course: courses.sample)
-
-# end
-# questions = Question.all
-
-# 75.times do
-#   Response.create(content: Faker::Lorem.paragraph, user: users.sample, question: questions.sample )
-# end
-
-# responses = Response.all
-
-150.times do
-  venue = Venue.find(rand(Venue.count)+1)
-  base_time = Faker::Time.forward(rand(60)+1, :evenin g)
-  Meetup.create(title: Faker::Company.name, description: Faker::Hacker.say_something_smart, course: courses.sample, organizer: users.sample, venue: venue, remote: true, latitude: venue.latitude, longitude: venue.longitude, scheduled_date: base_time, begin_time: base_time, end_time: base_time + 3600)
-  sleep 0.2
-end
-
-
-meetups = Meetup.all
-
-# ADD WAy MORE MEETUP MEMBERSHIPS
-# 50.times do
-#   MeetupMembership.create(meetup_id: meetups.sample.id, user_id: users.sample.id)
-# end
 
 User.all.each do |user|
   rand(1..3).times do
@@ -170,19 +121,6 @@ users.each do |user|
                         user_token: "user_token" + number,
                         user_secret: "user_secret" + number)
 end
-
-
-
-sarah = User.create(first_name:"Sarah", last_name: "Ing", email:"chocolate@gmail.com", password:"chocolate", street_address: "351 West Hubbard Street", city: "Chicago", state: "IL", zip: 60654, max_distance: 5, bio: "I am eager to see what this programming business is about! Willing to learn from anyone with more experience.")
-matthew = User.create(first_name:"Matthew", last_name: "Who", email:"matthewwho@me.com", password:"password", street_address: "351 West Hubbard Street", city: "Chicago", state: "IL", zip: 60654, max_distance: 5, bio: bio)
-john = User.create(first_name:"John", last_name: "Schulz", email:"john@gmail.com", password:"password", street_address: "351 West Hubbard Street", city: "Chicago", state: "IL", zip: 60654, max_distance: 5, bio: "I like candy!")
-daniel = User.create(first_name:"Daniel", last_name: "Hanson", email:"daniel@gmail.com", password:"password", street_address: "351 West Hubbard Street", city: "Chicago", state: "IL", zip: 60654, max_distance: 5, bio: "I am an experienced web developer and am passionate about helping budding programmers!")
-User.create(first_name:"test", last_name: "test", email:"test@test.com", password:"password", street_address: "351 West Hubbard Street", city: "Chicago", state: "IL", zip: 60654, max_distance: 5, bio: bio)
-
-venue = Venue.find(rand(Venue.count)+1)
-venue1 = venues_array.sample
-
-ruby_newbies = Meetup.create(title: "Ruby For Newbies", course: newbies, description: "This is our first meetup for beginners who are interested in learning Ruby. We will take the time to go over some programming basics and answer any questions you have. Food will be provided!", organizer: daniel, venue: venue, remote: true, latitude: venue.latitude, longitude: venue.longitude, scheduled_date: Time.now, begin_time: Time.now, end_time: Time.now + rand(1..4).days)
 
 [matthew, john, sarah].each do |person|
   ruby_newbies.users << person
